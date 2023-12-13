971a0d851dbd5ddda4edea69c652cf9967d5eea6
alfacentr83 <alfacentr83@gmail.com>
2023-12-13 19:58:26 +0100

    add Person class

---
+++ src/Person.java
@@ -0,0 +1,15 @@
+public class Person {
+    public String firstName;
+    public String lastName;
+    public Person(String firstName,String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    public String info(){
+      return firstName + "" + lastName;
+
+    }
+
+
+
+}
